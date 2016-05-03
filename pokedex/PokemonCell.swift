02ff4aed06847d65ca414bc9b2ec6eb003647771
Hans Hazairi <hnshzr@hotmail.com>
2016-05-04 01:23:43 +0800

    added custom pokecell class

---
+++ pokedex/PokemonCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonCell.swift
+//  pokedex
+//
+//  Created by Hans Hazairi on 04/05/2016.
+//  Copyright © 2016 Hans Hazairi. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCell: UICollectionViewCell {
+    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet weak var pokemonName: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell (pokemon: Pokemon) {
+        self.pokemon = pokemon
+        pokemonName.text = self.pokemon.name.capitalizedString
+        pokemonImage.image = UIImage(named: "\(self.pokemon.id)")
+    }
+    
+    
+}

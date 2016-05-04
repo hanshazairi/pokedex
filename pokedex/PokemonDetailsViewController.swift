7a46c567de5e880085573479c0393baf851fbb7b
Hans Hazairi <hnshzr@hotmail.com>
2016-05-04 18:31:59 +0800

    added pokemondetailsviewcontroller and created the segue

---
+++ pokedex/PokemonDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PokemonDetailsViewController.swift
+//  pokedex
+//
+//  Created by Hans Hazairi on 04/05/2016.
+//  Copyright © 2016 Hans Hazairi. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
